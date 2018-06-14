33a273e8ba779e71c4e5673e0436e6475c34da89
Abhinavlamba <abhinavlamba826@gmail.com>
2018-06-14 17:49:35 +0530

    hollywood

--- app/src/main/java/com/travis/android/wv/video.java
+++ app/src/main/java/com/travis/android/wv/video.java
@@ -17,7 +17,7 @@ public class video extends Activity {
         setContentView(R.layout.video);
         VideoView mVideoView = (VideoView) findViewById(R.id.video);
         mVideoView.setMediaController(new MediaController(this));
-        String viewSource ="http://wapbestmovie.xyz/load/Full_Movies/Bollywood_Movies_2018/Padman%202018%20Dvdrip/MP4%20HQ/Padman%202018%20Dvdrip%20Hq%20Part1.mp4";
+        String viewSource ="http://dl1.upload08.com/files/Film/2018/Backstabbing%20for%20Beginners%202018/Backstabbing.for.Beginners.2018.720p.BRrip.YIFY.FardaDL.mkv";
         mVideoView.setVideoURI(Uri.parse(viewSource));
     }
 }
