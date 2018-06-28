89ddfee655be5742c202675f3edd064c50f7d9c1
ThisIsNSH <hadanis.singh@gmail.com>
2018-06-28 11:19:17 +0530

    list picker

---
+++ app/src/main/java/com/travis/movie/AdvancedExampleCountryAdapter.java
@@ -0,0 +1,70 @@
+package com.travis.movie;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by ThisIsNSH on 6/28/2018.
+ */
+public class AdvancedExampleCountryAdapter extends ArrayAdapter<AdvancedExampleCountryPOJO> {
+    private List<AdvancedExampleCountryPOJO> items;
+    private Context context;
+
+    public AdvancedExampleCountryAdapter(@NonNull Context context, int resource, @NonNull List<AdvancedExampleCountryPOJO> objects) {
+        super(context, resource, objects);
+        this.items = objects;
+        this.context = context;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        AdvancedExampleCountryAdapter.ViewHolder holder;
+
+        if (null == convertView) {
+            LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            assert vi != null;
+            convertView = vi.inflate(R.layout.advanced_example_country_list_item, parent, false);
+            holder = new AdvancedExampleCountryAdapter.ViewHolder();
+            holder.text = convertView.findViewById(R.id.custom_cell_text);
+            holder.icon = convertView.findViewById(R.id.custom_cell_image);
+            convertView.setTag(holder);
+        } else {
+            holder = (AdvancedExampleCountryAdapter.ViewHolder) convertView.getTag();
+        }
+        if (null != holder) {
+            holder.text.setText(items.get(position).getTitle());
+            holder.icon.setImageResource(items.get(position).getIcon());
+        }
+        return convertView;
+    }
+
+    private class ViewHolder {
+        TextView text;
+        ImageView icon;
+    }
+}
---
+++ app/src/main/java/com/travis/movie/AdvancedExampleCountryPickerBox.java
@@ -0,0 +1,61 @@
+package com.travis.movie;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ramotion.directselect.DSAbstractPickerBox;
+
+/**
+ * Created by ThisIsNSH on 6/28/2018.
+ */
+public class AdvancedExampleCountryPickerBox extends DSAbstractPickerBox<AdvancedExampleCountryPOJO> {
+    private TextView text;
+    private ImageView icon;
+    private View cellRoot;
+
+    public AdvancedExampleCountryPickerBox(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public AdvancedExampleCountryPickerBox(@NonNull Context context,
+                                           @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AdvancedExampleCountryPickerBox(@NonNull Context context,
+                                           @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(@NonNull Context context) {
+        LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        assert mInflater != null;
+        mInflater.inflate(R.layout.advanced_example_country_picker_box, this, true);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        this.text = findViewById(R.id.custom_cell_text);
+        this.icon = findViewById(R.id.custom_cell_image);
+        this.cellRoot = findViewById(R.id.custom_cell_root);
+    }
+
+    @Override
+    public void onSelect(AdvancedExampleCountryPOJO selectedItem, int selectedIndex) {
+        this.text.setText(selectedItem.getTitle());
+        this.icon.setImageResource(selectedItem.getIcon());
+    }
+
+    @Override
+    public View getCellRoot() {
+        return this.cellRoot;
+    }
+}
