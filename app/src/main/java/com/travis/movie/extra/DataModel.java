8c396d87cfa7fa7966620fec2acddb3ec1940ef5
Abhinavlamba <abhinavlamba826@gmail.com>
2018-07-04 19:23:44 +0530

    NavDrawer added:)

---
+++ app/src/main/java/com/travis/movie/extra/DataModel.java
@@ -0,0 +1,11 @@
+package com.travis.movie.extra;
+
+public class DataModel {
+    public String name;
+
+    // Constructor.
+    public DataModel(String name) {
+
+        this.name = name;
+    }
+}
